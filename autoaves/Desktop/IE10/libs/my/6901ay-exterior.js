0933807312f77c65c672c4e17c065be5cdf387c2
KingWingsHome <kingwingshome@163.com>
2014-12-01 23:56:23 +0800

    IE10 #2

---
+++ autoaves/Desktop/IE10/libs/my/6901ay-exterior.js
@@ -0,0 +1,18 @@
+/**
+ * Created by admin on 2014/11/20.
+ */
+
+function init() {
+    initVars();
+    initEvents();
+}
+
+function initVars() {
+
+}
+
+function initEvents() {
+
+}
+
+$(document).ready(init);
---
+++ autoaves/Desktop/IE10/libs/my/c12-interior.js
@@ -0,0 +1,3 @@
+/**
+ * Created by admin on 2014/11/20.
+ */
